3bc613e071a8aa67281677dcf550f1a25f959ebe
Volodymyr Protsenko <vovapro01@gmail.com>
2020-06-02 16:49:49 +0300

    Check Style

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,6 +1,5 @@
 package project;
 
-import java.util.List;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import project.config.AppConfig;
 import project.model.User;
@@ -8,7 +7,8 @@ import project.service.UserService;
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
         UserService userService = context.getBean(UserService.class);
         userService.add(new User("Bob", "1234"));
         userService.add(new User("Alisa", "1234"));
