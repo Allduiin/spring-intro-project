043ba5a2f0d5f86289c053643cac5fdbbe496c15
Volodymyr Protsenko <vovapro01@gmail.com>
2020-06-06 12:53:16 +0300

    Deleted unused import

--- src/main/java/project/controllers/HelloController.java
+++ src/main/java/project/controllers/HelloController.java
@@ -2,7 +2,6 @@ package project.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import project.UserResponseDto;
 
 @RestController
 public class HelloController {
